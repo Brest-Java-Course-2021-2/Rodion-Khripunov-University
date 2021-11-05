674c72ae92fb5a441eff4383e5d7ba711741d8da
Rodion Khripunov <feeltheradiance@gmail.com>
2021-11-05 14:52:09 +0300

    multimodule structure

---
+++ model/src/main/java/com/epam/brest/model/Student.java
@@ -0,0 +1,64 @@
+package com.epam.brest.model;
+
+public class Student {
+
+    private Integer studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Integer course;
+
+    private Integer universityId;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    public Integer getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(Integer universityId) {
+        this.universityId = universityId;
+    }
+}
---
+++ model/src/main/java/com/epam/brest/model/University.java
@@ -0,0 +1,24 @@
+package com.epam.brest.model;
+
+public class University {
+
+    private Integer universityId;
+
+    private String universityName;
+
+    public Integer getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(Integer universityId) {
+        this.universityId = universityId;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+}
